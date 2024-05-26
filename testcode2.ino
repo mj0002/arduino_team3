3c99247931b4b9748b0156c705eb920a70fd9d61
choeyeonseo <106595772+choeyeonseo@users.noreply.github.com>
2024-05-26 17:10:28 +0900

    Update testcode2.ino

--- testcode2.ino
+++ testcode2.ino
@@ -1,24 +1,25 @@
 #include <Adafruit_Fingerprint.h>
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
+#include <SoftwareSerial.h>
 
-//지문인식 센서
-SoftwareSerial mySerial(2, 3);
+// 지문인식 센서
+SoftwareSerial mySerial(10, 11); // 초음파 센서와 충돌하지 않는 핀 번호로 변경
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
 uint8_t id;
 
-//lcd 센서
+// LCD 센서
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-//서보모터 센서
+// 서보모터 센서
 Servo high;
 int angle = 90;
 
-//초음파 센서 설정
-int trigPin = 2;
-int echoPin = 3;
+// 초음파 센서 설정
+int trigPin = 6;  // 충돌하지 않는 핀 번호로 변경
+int echoPin = 7;  // 충돌하지 않는 핀 번호로 변경
 
-//층별 정보 저장
+// 층별 정보 저장
 struct Employee {
   int id;
   int high;
@@ -33,8 +34,7 @@ Employee employees[] = {
 };
 
 void setup() {
-
-  //lcd 
+  // LCD 초기화
   lcd.init();
   lcd.backlight();
   lcd.begin(16, 2);
@@ -42,19 +42,19 @@ void setup() {
 
   Serial.begin(9600);
 
-  //초음파 센서
+  // 초음파 센서 
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
 
-  //서보모터
-  high.attach(12);
+  // 서보모터 
+  high.attach(12); 
 
-  //지문인식 센서 시작
+  // 지문인식 센서 시작
   while (!Serial);  // For Yun/Leo/Micro/Zero/...
   delay(100);
   Serial.println("\n\nAdafruit Fingerprint sensor enrollment");
 
-  // set the data rate for the sensor serial port
+  // 지문인식 센서 시리얼 포트 설정
   finger.begin(57600);
 
   if (finger.verifyPassword()) {
@@ -81,17 +81,15 @@ void setup() {
   }
   else {
     Serial.println("Waiting for valid finger...");
-      Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
+    Serial.print("Sensor contains "); Serial.print(finger.templateCount); Serial.println(" templates");
   }
- 
 }
 
-//시리얼 모니터에 지문 id를 입력
+// 시리얼 모니터에 지문 ID를 입력
 uint8_t readnumber(void) {
   uint8_t num = 0;
-
   while (num == 0) {
-    while (! Serial.available());
+    while (!Serial.available());
     num = Serial.parseInt();
   }
   return num;
@@ -99,27 +97,29 @@ uint8_t readnumber(void) {
 
 void loop() {
   Serial.println("---------------Menu--------------");
-  Serial.println("1.enroll, 2.fingerpring, 3.delete");
-  switch(readnumber()){
+  Serial.println("1.enroll, 2.fingerprint, 3.delete");
+  switch(readnumber()) {
     case 1:
       Serial.println("Ready to enroll a fingerprint!");
       Serial.println("Please type in the ID # (from 1 to 127) you want to save this finger as...");
       id = readnumber();
-      if (id == 0) {// ID #0 not allowed, try again!
-      return;
+      if (id == 0) { // ID #0 not allowed, try again!
+        return;
       }
       Serial.print("Enrolling ID #");
       Serial.println(id);
-      while (! getFingerprintEnroll() );
-    
+      while (!getFingerprintEnroll());
+      break;
+
     case 2:
       getFingerprintID();
       delay(100);
+      break;
 
     case 3:
       Serial.println("Please type in the ID # (from 1 to 127) you want to delete...");
       uint8_t id = readnumber();
-      if (id == 0) {// ID #0 not allowed, try again!
+      if (id == 0) { // ID #0 not allowed, try again!
         return;
       }
 
@@ -127,51 +127,50 @@ void loop() {
       Serial.println(id);
 
       deleteFingerprint(id);
-}
+      break;
+  }
 
-  if (dist() >0 && dist() < 10) {
+  // 초음파 센서를 이용한 거리 측정 및 서보 모터 제어
+  float distance = dist();
+  if (distance > 0 && distance < 10) {
     displayEmployee(employees[0]);
     delay(3000);
     currentFloor(employees[0]);
-
-    high.write(180);
-    delay(10);
-
-  }
-  if (dist() > 10 && dist() < 20) {
+    moveServo(employees[0].high);
+  } else if (distance > 10 && distance < 20) {
     displayEmployee(employees[1]);
     delay(3000);
     currentFloor(employees[1]);
-    high.write(360);
-    delay(10);
-
-  }
-  if (dist() > 20 && dist() < 30) {
+    moveServo(employees[1].high);
+  } else if (distance > 20 && distance < 30) {
     displayEmployee(employees[2]);
     delay(3000);
     currentFloor(employees[2]);
-    high.write(720);
-    delay(10); 
+    moveServo(employees[2].high);
   }
 
- 
+  // 초음파 센서로 거리를 다시 측정하여 1층으로 돌아가는 로직
+  distance = dist();
+  if (distance < 10) {
+    moveServo(0);  // 서보 모터를 1층 높이로 이동
+  }
 }
-//초음파 센서로 부터 받은 정보 중 거리값만 반환
-float dist(){
-  float dist, duration;
+
+// 초음파 센서로부터 받은 거리값 반환
+float dist() {
+  float distance, duration;
 
   digitalWrite(trigPin, HIGH);
   delay(10);
   digitalWrite(trigPin, LOW);
 
   duration = pulseIn(echoPin, HIGH);
+  distance = ((float)(340 * duration) / 10000) / 2;
 
-  dist = ((float)(340 * duration) / 10000) / 2;
-
-  return dist;
+  return distance;
 }
 
-//lcd에 멤버 표시
+// LCD에 멤버 정보 표시
 void displayEmployee(Employee employee) {
   lcd.clear();
   lcd.setCursor(0, 0);
@@ -186,9 +185,15 @@ void currentFloor(Employee employee) {
   lcd.print(employee.floor);
 }
 
-//지문 등록 코드 enroll
-uint8_t getFingerprintEnroll() {
+// 서보 모터를 지정된 높이로 이동
+void moveServo(int height) {
+  int angle = map(height, 0, 30, 0, 180); // 높이값을 각도로 매핑
+  high.write(angle);
+  delay(10);
+}
 
+// 지문 등록 코드 enroll
+uint8_t getFingerprintEnroll() {
   int p = -1;
   Serial.print("Waiting for valid finger to enroll as #"); Serial.println(id);
   while (p != FINGERPRINT_OK) {
@@ -213,7 +218,6 @@ uint8_t getFingerprintEnroll() {
   }
 
   // OK success!
-
   p = finger.image2Tz(1);
   switch (p) {
     case FINGERPRINT_OK:
@@ -267,7 +271,6 @@ uint8_t getFingerprintEnroll() {
   }
 
   // OK success!
-
   p = finger.image2Tz(2);
   switch (p) {
     case FINGERPRINT_OK:
@@ -282,7 +285,7 @@ uint8_t getFingerprintEnroll() {
     case FINGERPRINT_FEATUREFAIL:
       Serial.println("Could not find fingerprint features");
       return p;
-    case FINGERPRINT_INVALIDIMAGE:
+        case FINGERPRINT_INVALIDIMAGE:
       Serial.println("Could not find fingerprint features");
       return p;
     default:
@@ -328,80 +331,55 @@ uint8_t getFingerprintEnroll() {
   return true;
 }
 
-//등록된 지문 정보 확인 Fingerprint
-
+// 등록된 지문 정보 확인 Fingerprint
 uint8_t getFingerprintID() {
   uint8_t p = finger.getImage();
   switch (p) {
     case FINGERPRINT_OK:
-      //Serial.println("Image taken");
       break;
     case FINGERPRINT_NOFINGER:
-      //Serial.println("No finger detected");
       return p;
     case FINGERPRINT_PACKETRECIEVEERR:
-      //Serial.println("Communication error");
       return p;
     case FINGERPRINT_IMAGEFAIL:
-      //Serial.println("Imaging error");
       return p;
     default:
-      //Serial.println("Unknown error");
       return p;
   }
-
-  // OK success!
 
   p = finger.image2Tz();
   switch (p) {
     case FINGERPRINT_OK:
-      //Serial.println("Image converted");
       break;
     case FINGERPRINT_IMAGEMESS:
-      //Serial.println("Image too messy");
       return p;
     case FINGERPRINT_PACKETRECIEVEERR:
-      //Serial.println("Communication error");
       return p;
     case FINGERPRINT_FEATUREFAIL:
-      //Serial.println("Could not find fingerprint features");
       return p;
     case FINGERPRINT_INVALIDIMAGE:
-      //Serial.println("Could not find fingerprint features");
       return p;
     default:
-      //Serial.println("Unknown error");
       return p;
   }
 
-  // OK converted!
   p = finger.fingerSearch();
   if (p == FINGERPRINT_OK) {
-    //Serial.println("Found a print match!");
   } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
-    //Serial.println("Communication error");
     return p;
   } else if (p == FINGERPRINT_NOTFOUND) {
-    //Serial.println("Did not find a match");
     return p;
   } else {
-    //Serial.println("Unknown error");
     return p;
   }
 
-  // found a match!
-  //lcd 표시 
-  displayEmployee(employees[finger.fingerID]);
-
-  
-
-  //Serial.print("Found ID #"); Serial.print(finger.fingerID);
-  //Serial.print(" with confidence of "); Serial.println(finger.confidence);
+  // lcd 표시
+  displayEmployee(employees[finger.fingerID - 1]);  // ID는 1부터 시작하므로 배열 인덱스를 맞추기 위해 -1
 
   return finger.fingerID;
 }
 
-//지문 삭제 delete
+// 지문 삭제 delete
 uint8_t deleteFingerprint(uint8_t id) {
   uint8_t p = -1;
 
@@ -421,3 +399,4 @@ uint8_t deleteFingerprint(uint8_t id) {
 
   return p;
 }
+
